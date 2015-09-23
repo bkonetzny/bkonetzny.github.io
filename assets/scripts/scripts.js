1d7adf994945cb9a8f64801eff409a893957fd68
Bastian Konetzny <b.konetzny@organize.me>
2015-09-23 19:10:50 +0200

    Added Drupal card and location.hash handling.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,22 +1,35 @@
-$('.card-toggle').on('click', function(){
-  var $toggle = $(this);
-  var isAnimating = false;
+$(document).ready(function(){
+  var showCard = function(card){
+    var isAnimating = false;
+    var $card = $('#' + card);
 
-  $('.card-toggle').removeClass('active');
-  $toggle.addClass('active');
+    $('.card-toggle').removeClass('active');
+    $card.find('.card-toggle').addClass('active');
 
-  if (!isAnimating) {
-    isAnimating = true;
+    if (!isAnimating) {
+      isAnimating = true;
 
-    $('.card').removeClass('active');
+      $('.card').removeClass('active');
 
-    setTimeout(function(){
-      $toggle.parent().toggleClass('active').find('.card-content').on('transitionend', function(){
-        isAnimating = false;
-      });
-    }, 10);
-  }
-  else {
-    return;
-  }
+      setTimeout(function(){
+        $card.toggleClass('active').find('.card-content').on('transitionend', function(){
+          isAnimating = false;
+        });
+      }, 10);
+    }
+    else {
+      return;
+    }
+  };
+
+  var parseHash = function(hash){
+    var card = hash.split('/').pop() || 'overview';
+    showCard(card);
+  };
+
+  parseHash(location.hash);
+
+  $(window).bind('hashchange', function() {
+    parseHash(location.hash);
+  });
 });
